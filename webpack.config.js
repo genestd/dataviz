586ccb4a3295942c58aedb4489b89bc3b921e221
genestd <drwgenest@gmail.com>
2017-02-21 16:40:26 -0800

    stackoverflow

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports={
   ],
   output: {
     path: __dirname + '/dist',
-    //publicPath: './dist',
+    publicPath: 'https://genestd.github.io/dataviz/',
     filename: "index_bundle.js"
   },
   devServer: {
